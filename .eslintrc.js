7a4f1516bc6ab8565be99ac148cf03695c1ebe25
DaniFoldi <dani@stefan.hu>
2022-02-03 09:30:42 +0000

    Better linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ['@typescript-eslint'],
+  plugins: [ '@typescript-eslint' ],
   // Prerequisite `eslint-plugin-vue`, being extended, sets
   // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
   // in turn delegates to the parser, specified in `parserOptions.parser`:
@@ -7,143 +7,71 @@ module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    extraFileExtensions: ['.vue'],
+    extraFileExtensions: [ '.vue' ],
     ecmaFeatures: { jsx: true },
     ecmaVersion: 2021,
     sourceType: 'module'
   },
-  ignorePatterns: ['node_modules/'],
+  env: { 'vue/setup-compiler-macros': true },
+  ignorePatterns: [ 'node_modules/' ],
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
     'plugin:nuxt/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended'
   ],
-  overrides: [{
-    files: ['*.ts', '*.json', '*.js', 'build/*'],
-    env: { node: true }
-  }, {
-    files: ['*.ts', '*.tsx'],
-    rules: {
+  overrides: [
+    {
+      files: [ '*.ts', '*.json', '*.js', 'build/*' ],
+      env: { node: true }
+    }, {
+      files: [ '*.ts', '*.tsx' ],
+      rules: {
       // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
       // does not work with type definitions
-      'no-unused-vars': 'off'
-    }
-  }],
-  rules: {
-    'block-spacing': [
-      'error',
-      'always'
-    ],
-    'brace-style': [
-      'error',
-      '1tbs'
-    ],
-    'comma-dangle': [
-      'warn',
-      'never'
-    ],
-    'consistent-return': 'off',
-    'eol-last': [
-      'error',
-      'always'
-    ],
-    'function-paren-newline': [
-      'error',
-      'multiline'
-    ],
-    'indent': [
-      'error',
-      2,
-      { 'SwitchCase': 1 }
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'lines-between-class-members': [
-      'error',
-      'always',
-      { 'exceptAfterSingleLine': true }
-    ],
-    'max-len': [
-      'error',
-      120
-    ],
-    'no-else-return': 'off',
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        'max': 1,
-        'maxEOF': 0
+        'no-unused-vars': 'off'
       }
-    ],
+    }
+  ],
+  rules: {
+    'array-bracket-newline': [ 'warn', { 'multiline': true }],
+    'array-bracket-spacing': [ 'warn', 'always', { 'objectsInArrays': false, 'arraysInArrays': false }],
+    'arrow-parens': [ 'warn', 'as-needed' ],
+    'arrow-spacing': [ 'warn' ],
+    'block-spacing': [ 'warn', 'always' ],
+    'brace-style': [ 'error', '1tbs' ],
+    'comma-dangle': [ 'warn', 'never' ],
+    'comma-spacing': [ 'warn', { 'before': false, 'after': true }],
+    'consistent-return': 'off',
+    'eol-last': [ 'error', 'always' ],
+    'eqeqeq': [ 'error', 'always' ],
+    'func-call-spacing': [ 'error', 'never' ],
+    'function-paren-newline': [ 'error', 'multiline' ],
+    'indent': [ 'error', 2, { 'SwitchCase': 2 }],
+    'linebreak-style': [ 'error', 'unix' ],
+    'lines-between-class-members': [ 'error', 'always', { 'exceptAfterSingleLine': true }],
+    'key-spacing': [ 'warn', { 'beforeColon': false, 'afterColon': true, 'mode': 'strict' }],
+    'keyword-spacing': [ 'warn', { 'before': true, 'after': true }],
+    'max-len': [ 'error', 120 ],
+    'no-else-return': 'off',
+    'no-multiple-empty-lines': [ 'error', { 'max': 1, 'maxEOF': 0 }],
     'no-trailing-spaces': 'error',
     'no-var': 'error',
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
     'object-curly-newline': [
       'error',
-      { 'multiline': true }
-    ],
-    'object-curly-spacing': [
-      'error',
-      'always'
-    ],
-    'func-call-spacing': [
-      'error',
-      'never'
-    ],
-    'operator-linebreak': [
-      'error',
-      'before'
-    ],
-    'arrow-parens': [
-      'error',
-      'as-needed'
-    ],
-    'space-in-parens': [
-      'error',
-      'never'
-    ],
-    'sort-imports': ['error'],
-    'key-spacing': [
-      'warn',
       {
-        'beforeColon': false,
-        'afterColon': true,
-        'mode': 'strict'
+        'ImportDeclaration': 'never',
+        'ExportDeclaration': { 'multiline': true, 'minProperties': 3 }
       }
     ],
-    'comma-spacing': [
-      'warn',
-      {
-        'before': false,
-        'after': true
-      }
-    ],
-    'keyword-spacing': [
-      'warn',
-      {
-        'before': true,
-        'after': true
-      }
-    ],
-    'arrow-spacing': [
-      'error'
-    ],
-    'space-infix-ops': [
-      'error'
-    ],
-    'vue/multi-word-component-names': [
-      'off'
-    ],
+    'object-curly-spacing': [ 'warn', 'always' ],
+    'operator-linebreak': [ 'warn', 'before' ],
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'never' ],
+    'sort-imports': [ 'warn', { 'ignoreCase': true }],
+    'space-in-parens': [ 'warn', 'never' ],
+    'space-infix-ops': [ 'warn' ],
+    'vue/multi-word-component-names': [ 'off' ],
     'vue/max-attributes-per-line': [
       'warn',
       { 'singleline': 3, 'multiline': 1 }
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  'plugins': [
-    'stylelint-scss'
-  ],
+  'plugins': [ 'stylelint-scss' ],
   'rules': {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'selector-pseudo-element-no-unknown': [
       true,
-      { ignorePseudoElements: ['v-deep'] }
+      { ignorePseudoElements: [ 'v-deep' ] }
     ]
   },
   'extends': 'stylelint-config-recommended-scss',
