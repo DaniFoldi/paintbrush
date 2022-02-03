0df3db82d0ac07de2e5bf3878cf7e29de5cc0827
DaniFoldi <dani@stefan.hu>
2022-02-03 10:36:03 +0000

    Add env to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: [
-    '~/build/componentDocs.ts'
-  ]
+  buildModules: [ '~/build/componentDocs.ts' ],
+  publicRuntimeConfig: { ENV: process.env.NODE_ENV }
 })
