6029ede059ddffbba808c4c4e3ca1a4767149431
DaniFoldi <dani@stefan.hu>
2022-02-17 22:13:15 +0000

    Dear Vue 3.2, please do not complain about an upcoming feature

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+import { useNuxtApp } from '#app'
+useNuxtApp().vueApp.config.unwrapInjectedRef = true
+</script>
