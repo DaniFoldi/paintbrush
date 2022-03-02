9ce5179ade9a4b879f4b142f16efed62225058fa
DaniFoldi <dani@stefan.hu>
2022-03-02 22:01:52 +0000

    Add Paintbrush docs page

--- app.vue
+++ app.vue
@@ -1,7 +1,42 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="container">
+    <Paintbrush
+      app-name="Paintbrush UI Docs"
+      canonical="https://paintbrush.danifoldi.com"
+      description="View the documentation for the Paintbrush UI component library"
+      favicon="paintbrush.png"
+      large-image="paintbrush.png"
+      large-image-alt="Paintbrush UI Logo"
+      page-title="Paintbrush UI Docs"
+      twitter-handle="danifoldi"
+      url="https://paintbrush.danifoldi.com"
+    >
+      <Sidebar>
+        <template #sidebar-middle>
+          <SidebarEntry href="/" icon="rocket-launch">
+            Welcome
+          </SidebarEntry>
+          <SidebarEntry color="green" href="/tutorial" icon="code">
+            Basic Use
+          </SidebarEntry>
+          <SidebarEntry color="orange" href="/components" icon="flask">
+            Components
+          </SidebarEntry>
+          <SidebarEntry
+            color="purple"
+            href="https://github.com/DaniFoldi/Paintbrush"
+            icon="github-logo"
+            :reload="true"
+          >
+            Source
+          </SidebarEntry>
+        </template>
+        <template #content>
+          <NuxtPage />
+        </template>
+      </Sidebar>
+    </Paintbrush>
+  </div>
 </template>
 
 <script lang="ts" setup>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <Paintbrush>
-    <h1>test</h1>
-    <TestComponent />
-  </Paintbrush>
+  <div>
+    <ThemeSwitch />
+    <Text>asd</Text>
+    <Icon name="gear" />
+    <AutoLink href="https://paintbrush.danifoldi.com" :reload="true">
+      link
+    </AutoLink>
+    <PrimaryButton>
+      button
+    </PrimaryButton>
+    <Checkbox>Test</Checkbox>
+  </div>
 </template>
-
-<script lang="ts">
-export default {  }
-</script>
