025a99a1058008a3b688fd9f22aafc7b6720d31a
DaniFoldi <dani@stefan.hu>
2022-03-02 22:03:29 +0000

    Add components and tutorial pages

---
+++ pages/components.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div v-for="component in components" :key="component.name">
+      <h3>{{ component.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { data: components } = await useFetch('/docs.json')
+</script>
