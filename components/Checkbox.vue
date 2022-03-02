3ae006e666b20f486b0a61067cad156c59156465
DaniFoldi <dani@stefan.hu>
2022-03-02 22:08:08 +0000

    Add basic checkbox

---
+++ components/Checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <label>
+    <input v-model="checked" type="checkbox">
+    <Text><slot /></Text>
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/mixins.scss';
+
+label {
+  @include mixins.two-items;
+}
+</style>
