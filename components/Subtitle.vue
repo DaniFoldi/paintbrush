4dcb4a59ef0710390be4705b632999739d2535f4
DaniFoldi <dani@stefan.hu>
2022-03-06 20:25:41 +0000

    Add Title and Subtitle components

---
+++ components/Subtitle.vue
@@ -0,0 +1,18 @@
+<!--!
+@name Subtitle
+@version 1.0.0
+@description Subtitle component to display subtitles
+@usage <Subtitle>Hello</Subtitle>
+-->
+<template>
+  <h2><slot /></h2>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/mixins';
+
+h2 {
+  @include mixins.vertical-unit-margin;
+  @include mixins.standard-text;
+}
+</style>
---
+++ components/Title.vue
@@ -0,0 +1,18 @@
+<!--!
+@name Title
+@version 1.0.0
+@description Title component to display titles
+@usage <Title>Hello</Title>
+-->
+<template>
+  <h1><slot /></h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/mixins';
+
+h1 {
+  @include mixins.vertical-unit-margin;
+  @include mixins.standard-text;
+}
+</style>
