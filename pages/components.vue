bbca19cc8542ec0139ccce337c2c0cae929907b8
DaniFoldi <dani@stefan.hu>
2022-03-06 20:27:05 +0000

    Work on component docs engine

--- pages/components.vue
+++ pages/components.vue
@@ -2,10 +2,28 @@
   <div>
     <div v-for="component in components" :key="component.name">
       <h3>{{ component.name }}</h3>
+      <Text>{{ component.version }}</Text>
+      <Text>{{ component.description }}</Text>
+      <MultilineCode :code="component.usage" language="html" />
+      <Text v-for="related in component.see" :key="related">
+        <AutoLink :href="related">
+          {{ related }}
+        </AutoLink>
+      </Text>
+      <MultilineCode
+        v-for="example in component.example"
+        :key="example"
+        :code="example"
+        language="html"
+      />
+      <Text>{{ component.note }}</Text>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { data: components } = await useFetch('/docs.json')
+import type { ComponentDocs } from '../build/component-docs'
+
+
+const { data: components } = useFetch<ComponentDocs[]>('/docs.json')
 </script>
