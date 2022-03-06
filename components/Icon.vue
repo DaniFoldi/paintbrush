f2ae2649eebd99306a4353dec7f5b803fa844726
DaniFoldi <dani@stefan.hu>
2022-03-06 20:26:41 +0000

    Fix Icon examples and usage

--- components/Icon.vue
+++ components/Icon.vue
@@ -2,11 +2,11 @@
 @name Icon
 @version 1.0.0
 @description Icon component from Phosphor Icons
-@usage <Icon name="gear">
+@usage <Icon name="gear" />
 @property required; name: Icon name; type: String; description: Icon name as it appears in Phosphor Icons
 @property default: small; name: size; type: ['small', 'medium', 'large', 'x-large']; description: Font size of the icon
-@example <Icon name="chat-dots" size="large">
-@example <Icon name="mouse" size="medium">
+@example <Icon name="chat-dots" size="large" />
+@example <Icon name="mouse" size="medium" />
 @note For an icon list see https://phosphoricons.com/
 -->
 <template>
