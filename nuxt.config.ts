af31c6ef655aa53cf6d6c61676de1af0ce883f6d
DaniFoldi <dani@stefan.hu>
2022-04-10 21:49:13 +0200

    Add color-mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // TODO remove docs module once useFetch works properly
 
 export default defineNuxtConfig({
-  modules: [ './modules/docs' ],
+  colorMode: {},
+  modules: [ './modules/docs', '@nuxtjs/color-mode' ],
   publicRuntimeConfig: { ENV: process.env.NODE_ENV }
 })
