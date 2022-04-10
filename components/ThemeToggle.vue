da3b1184ee64d743f96950170494e299d4b37084
DaniFoldi <dani@stefan.hu>
2022-04-10 12:06:47 +0200

    Replace injects with composables

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -3,23 +3,18 @@
 @version 1.0.0
 -->
 <template>
-  <IconButton :icon="icon" @click="$emit('toggle'); toggle()">
+  <IconButton :icon="theme.includes('dark') ? 'moon' : 'sun'" @click="$emit('toggle'); toggle()">
     Switch
   </IconButton>
 </template>
 
 <script lang="ts" setup>
-import { inject } from 'vue'
-
-
 defineEmits([ 'toggle' ])
 
-let theme = inject('theme', 'dark')
-let icon = 'sun'
+const theme = useTheme()
 
 function toggle() {
-  theme = theme.split('dark').map((x: string) => x.replaceAll('light', 'dark')).join('light')
+  theme.value = theme.value.split('dark').map((x: string) => x.replaceAll('light', 'dark')).join('light')
   console.log(theme)
-  icon = theme.includes('dark') ? 'moon' : 'sun'
 }
 </script>
