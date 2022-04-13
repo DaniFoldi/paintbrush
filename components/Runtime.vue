2fddd325892016702d8f224a01d583fc59decf32
DaniFoldi <dani@stefan.hu>
2022-04-13 21:06:24 +0200

    :construction: Add test runtime rendering feature

---
+++ components/Runtime.vue
@@ -0,0 +1,16 @@
+<template>
+  <VNode />
+</template>
+
+<script setup lang="ts">
+  import { h } from 'vue'
+
+
+  interface RuntimeProps {
+    class: string
+    rendered: string
+  }
+
+  const props = defineProps<RuntimeProps>()
+  const VNode = () => h('div', { class: props.class, innerHTML: props.rendered })
+</script>
