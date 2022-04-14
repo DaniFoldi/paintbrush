a4b358dbcac51f58f8d6bde77292af84b220eceb
DaniFoldi <dani@stefan.hu>
2022-04-14 13:18:54 +0200

    :wrench: Initial i18n setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // TODO remove docs module once useFetch works properly
 
 export default defineNuxtConfig({
-  modules: [ './modules/docs' ],
+  i18n: {},
+  modules: [ './modules/docs', '@nuxtjs/i18n' ],
   publicRuntimeConfig: { ENV: process.env.NODE_ENV }
 })
