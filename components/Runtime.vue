18f406aaf788c9225e7f80f8b5b1280fb0e954c3
DaniFoldi <dani@stefan.hu>
2022-04-19 17:33:54 +0200

    :memo: Add docs for Runtime component

--- components/Runtime.vue
+++ components/Runtime.vue
@@ -1,3 +1,17 @@
+<!--!
+  @name Runtime
+  @version 1.0.0
+  @description Render any html at runtime
+  @usage <Runtime element="h1" html="Hello World" />
+  @property
+  class?: string [''] (CSS classes to apply to the rendered html)
+  html: string (Html to render)
+  @renderedExample
+  <Runtime html="Hello World" />
+  @renderedExample
+  <Runtime element="p" html="Some <em>text</em>" class="important" />
+  @note Nuxt does not process the given html
+-->
 <template>
   <VNode />
 </template>
