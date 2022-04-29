13fc422934a61f8bc259faca4a7e678f7285b70f
DaniFoldi <dani@stefan.hu>
2022-04-29 18:31:37 +0100

    :wrench: Use pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,5 @@ export default defineNuxtConfig({
     langDir: 'lang/',
     lazy: true
   },
-  modules: [ './modules/icons', './modules/docs' ]
+  modules: [ './modules/icons', './modules/docs', '@pinia/nuxt' ]
 })
