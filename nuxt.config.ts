6556154f545a4456a40a9544b37ff0fd256643bd
DaniFoldi <dani@stefan.hu>
2022-04-29 20:39:21 +0100

    :hammer: Fix nitro build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // #MARK(PB-2)
 
 export default defineNuxtConfig({
+  // #MARK nuxt/framework #4325
+  alias: {
+    '@vue/devtools-api': '@vue/devtools-api'
+  },
   i18n: {
     defaultLocale: 'en',
     langDir: 'lang/',
