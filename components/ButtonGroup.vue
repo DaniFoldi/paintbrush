5bba9750e712ff3d5f275ca98bc91bf7cc42e8d9
DaniFoldi <dani@stefan.hu>
2022-04-30 09:53:45 +0100

    :zap: Add button group component

---
+++ components/ButtonGroup.vue
@@ -0,0 +1,19 @@
+<!--!
+  @name ButtonGroup
+  @version 1.0.0
+  @icon arrows-in-line-vertical
+-->
+<template>
+  <div class="button-group">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div.button-group {
+    display: inline-grid;
+    gap: calc(var(--unit) * 2);
+    grid-auto-columns: auto;
+    grid-auto-flow: column;
+  }
+</style>
