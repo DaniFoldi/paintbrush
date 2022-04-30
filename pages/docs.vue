74c7619c88091964ae345b86f775876f5636906a
DaniFoldi <dani@stefan.hu>
2022-04-30 10:26:04 +0100

    :memo: Display components under categories

--- pages/docs.vue
+++ pages/docs.vue
@@ -1,21 +1,26 @@
 <template>
   <SidebarContainer>
     <template #sidebar-middle>
-      <SidebarLink
-        v-for="(component, path) in components"
-        :key="component.name"
-        :href="path.replace(/^components/, '/docs/components').replace(/\.vue$/, '')"
-        :icon="component.icon"
-        :icon-color="`${$route.path}.vue` === `/docs/${path}` ? 'var(--secondary)' : 'var(--text)'"
-        :icon-style="`${$route.path}.vue` === `/docs/${path}` ? 'bold' : 'regular'"
-      >
-        <Glue split>
-          <Text>{{ component.name }}</Text>
-          <Text light>
-            {{ component.version }}
-          </Text>
-        </Glue>
-      </SidebarLink>
+      <template v-for="(category) in categories" :key="category">
+        <SidebarCategory v-if="category">
+          {{ category }}
+        </SidebarCategory>
+        <SidebarLink
+          v-for="(component, path) in categorized[category]"
+          :key="component.name"
+          :href="path.replace(/^components/, '/docs/components').replace(/\.vue$/, '')"
+          :icon="component.icon"
+          :icon-color="`${$route.path}.vue` === `/docs/${path}` ? 'var(--secondary)' : 'var(--text)'"
+          :icon-style="`${$route.path}.vue` === `/docs/${path}` ? 'bold' : 'regular'"
+        >
+          <Glue split>
+            <Text>{{ component.name }}</Text>
+            <Text light>
+              {{ component.version }}
+            </Text>
+          </Glue>
+        </SidebarLink>
+      </template>
     </template>
     <template #content>
       <NuxtChild />
@@ -24,9 +29,19 @@
 </template>
 
 <script lang="ts" setup>
-  import type { Docs } from '../scripts/docs'
+  import type { Component, Docs } from '../scripts/docs'
 
 
   const { data } = await useFetch<Docs>('/api/docs')
-  const components = data.value
+  const docs: Docs = typeof data.value === 'string' ? JSON.parse(data.value) : data.value
+  const categorized: Record<string, Record<string, Component>> = {}
+  for (const path in docs) {
+    const component = docs[path]
+    const category = component.category
+    if (!categorized[category]) {
+      categorized[category] = {}
+    }
+    categorized[category][path] = component
+  }
+  const categories = Object.keys(categorized).sort((a, b) => a.localeCompare(b))
 </script>
