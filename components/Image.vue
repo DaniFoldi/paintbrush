5599e6cecd948e0c95b6aec8f310185134768b3a
DaniFoldi <dani@stefan.hu>
2022-05-01 19:12:05 +0100

    :zap: Add image component

---
+++ components/Image.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :alt="alt" :src="source">
+</template>
+
+<script lang="ts" setup>
+  interface ImageProps {
+    alt: string
+    maxWidth?: string
+    maxHeight?: string
+    source: string
+  }
+
+  const props = withDefaults(defineProps<ImageProps>(), {
+    maxHeight: '100%',
+    maxWidth: '100%'
+  })
+</script>
+
+<style lang="scss" scoped>
+  @use '../assets/mixins.scss';
+
+  img {
+    max-height: v-bind('props.maxHeight');
+    max-width: v-bind('props.maxWidth');
+  }
+</style>
