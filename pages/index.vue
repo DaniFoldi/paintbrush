14dc421072ff3d4d516948524b989b2b1c374a3f
DaniFoldi <dani@stefan.hu>
2022-05-01 17:02:56 +0100

    :memo: Add more test components

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,11 @@
     </ButtonGroup>
     <Checkbox>Test</Checkbox>
     <Form>
-      <Input label="Username" />
+      <Input label="Text" type="text" />
+      <Input label="Email" type="email" />
+      <Input label="Datetime" type="datetime" />
+      <Input label="Tel" type="tel" />
+      <Input label="Number" type="number" />
       <Input label="Password" type="password" />
     </Form>
   </Container>
