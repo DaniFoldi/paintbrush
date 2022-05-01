c7996de5d0b5a7e12c655f749aa0c1c6be615eed
DaniFoldi <dani@stefan.hu>
2022-05-01 19:12:52 +0100

    :memo: Create a separate test page for tests

---
+++ pages/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <Container padded>
+    <ThemeToggle />
+    <Text>asd</Text>
+    <Separator />
+    <Icon name="gear" />
+    <AutoLink href="https://paintbrush.danifoldi.com" reload>
+      link
+    </AutoLink>
+    <ButtonGroup>
+      <Button color="primary">
+        button
+      </Button>
+      <Container :max="false" right-badge="5">
+        <Button capitalize color="secondary">
+          second button
+        </Button>
+      </Container>
+      <Button color="primary" gradient="secondary">
+        gradient
+      </Button>
+      <Button ghost uppercase>
+        ghost
+      </Button>
+    </ButtonGroup>
+    <Checkbox>Test</Checkbox>
+    <Form>
+      <Input label="Text" type="text" />
+      <Input label="Email" type="email" />
+      <Input label="Datetime" type="datetime" />
+      <Input label="Tel" type="tel" />
+      <Input label="Number" type="number" />
+      <Input label="Password" type="password" />
+    </Form>
+  </Container>
+</template>
