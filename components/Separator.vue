e76306fdf2fd8b77eb69badb476743b2b468f3b8
DaniFoldi <dani@stefan.hu>
2022-05-01 16:36:17 +0100

    :zap: Add Separator component

---
+++ components/Separator.vue
@@ -0,0 +1,32 @@
+<!--!
+  @name Separator
+  @icon arrows-horizontal
+  @version 1.0.0
+-->
+<template>
+  <hr>
+</template>
+
+<script lang="ts" setup>
+  interface SeparatorProps {
+    spaced?: boolean
+  }
+
+  withDefaults(defineProps<SeparatorProps>(), {
+    spaced: false
+  })
+</script>
+
+<style lang="scss" scoped>
+  @use '../assets/mixins.scss';
+
+  hr {
+    border-color: var(--background-2);
+    border-radius: 1px;
+    @include mixins.double-unit-block-margin;
+
+    &.spaced {
+      @include mixins.unit-inline-margin;
+    }
+  }
+</style>
