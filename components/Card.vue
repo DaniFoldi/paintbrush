165deae350e1485d5f7578bc209aa7dcd1d76e43
DaniFoldi <dani@stefan.hu>
2022-05-03 16:28:51 +0100

    :zap: Add card component

---
+++ components/Card.vue
@@ -0,0 +1,30 @@
+<!--!
+  @name Card
+  @version 1.0.0
+-->
+<template>
+  <Container>
+    <slot />
+  </Container>
+</template>
+
+<script lang="ts" setup>
+  import { useThemeColor } from '#imports'
+
+
+  interface CardProps {
+    background?: string
+  }
+
+  const props = withDefaults(defineProps<CardProps>(), {
+    background: 'background'
+  })
+
+  const background = useThemeColor(props.background)
+</script>
+
+<style lang="scss" scoped>
+  div {
+    background: v-bind(background);
+  }
+</style>
