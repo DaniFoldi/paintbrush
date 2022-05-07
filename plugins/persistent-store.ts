391f28f4df4483516aa9592ab570fd29e30c1106
DaniFoldi <dani@stefan.hu>
2022-05-07 17:36:34 +0100

    :wrench: Setup persistent store

---
+++ plugins/persistent-store.ts
@@ -0,0 +1,7 @@
+import { createNuxtPersistedState } from 'pinia-plugin-persistedstate'
+import { defineNuxtPlugin, useCookie } from '#app'
+
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.$pinia.use(createNuxtPersistedState(useCookie))
+})
