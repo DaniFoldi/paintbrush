35e355e10b66c1069688d9cfed6cc65a21896880
DaniFoldi <dani@stefan.hu>
2022-05-08 10:33:27 +0100

    :zap: Add Breadcrumbs component

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,53 @@
+<!--!
+  @version 0.1.0
+  @icon path
+  @category Display
+-->
+<template>
+  <Container>
+    <Text
+      v-for="(piece, i) of path"
+      :key="piece.text + i"
+      inline
+      :underline="piece.underline"
+    >
+      <Text v-if="i !== 0" part>
+        /
+      </Text>
+      <Icon
+        v-if="piece.iconName"
+        :color="piece.iconColor ?? 'inherit'"
+        :name="piece.iconName"
+        :variant="piece.iconVariant ?? 'regular'"
+      />
+      {{ piece.text }}
+    </text>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+  import { IconTypes } from '../modules/icon-types'
+  import { IconVariants } from '../modules/icons'
+
+
+  defineProps<BreadcrumbsProps>()
+
+
+  interface Breadcrumb {
+    iconColor?: string
+    iconName: IconTypes
+    iconVariant?: IconVariants
+    text: string
+    underline?: boolean
+  }
+
+  interface BreadcrumbsProps {
+    path: Breadcrumb[]
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    grid-auto-flow: column;
+  }
+</style>
