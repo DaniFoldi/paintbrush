ed5a9eef0d9447a1548130535de941347ed45bb1
DaniFoldi <dani@stefan.hu>
2022-05-08 15:31:07 +0100

    :zap: Add menubars

---
+++ components/Menubar/Container.vue
@@ -0,0 +1,89 @@
+<!--!
+  @version 1.1.0
+  @category Navigation
+  @icon list
+-->
+<template>
+  <Container class="menubar-container" max>
+    <nav>
+      <div class="menubar-left">
+        <slot name="menubar-left" />
+      </div>
+      <div class="menubar-middle">
+        <slot name="menubar-middle" />
+      </div>
+      <div class="menubar-right">
+        <slot name="menubar-right" />
+      </div>
+    </nav>
+    <Container v-if="wrapContent" max>
+      <slot name="content" />
+    </Container>
+    <slot v-else name="content" />
+  </Container>
+</template>
+
+<script lang="ts" setup>
+  import Container from '../Container.vue'
+  import { useThemeColor } from '#imports'
+
+
+  interface ContainerProps {
+    menuElementSpacing?: string
+    menuPartSpacing?: string // Spacing between menubar parts
+    wrapContent?: boolean
+  }
+
+  const props = withDefaults(defineProps<ContainerProps>(), {
+    menuElementSpacing: 'unit',
+    menuPartSpacing: 'double-unit',
+    wrapContent: true
+  })
+
+
+  const element = useThemeColor(props.menuElementSpacing)
+  const part = useThemeColor(props.menuPartSpacing)
+</script>
+
+<style lang="scss" scoped>
+  @use '../../assets/mixins';
+
+  .menubar-container {
+    column-gap: v-bind(part);
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 64px 1fr;
+
+    & > div {
+      background: var(--background-2);
+    }
+  }
+
+  div.menubar-left, div.menubar-middle, div.menubar-right {
+    align-items: center;
+    column-gap: v-bind(element);
+    display: grid;
+    grid-auto-columns: max-content;
+    grid-auto-flow: column;
+  }
+
+  .menubar-right {
+    align-self: end;
+  }
+
+  nav {
+    background: var(--background);
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    overflow: hidden;
+    position: sticky;
+    top: 0;
+    z-index: 5;
+
+    @include mixins.with-fade;
+
+    & > div:not(:empty) {
+      @include mixins.double-unit-padding;
+    }
+  }
+</style>
---
+++ components/Menubar/Link.vue
@@ -0,0 +1,74 @@
+<template>
+  <AutoLink :class="{ 'with-icon': icon }" :href="href">
+    <Icon
+      v-if="icon"
+      center
+      :color="iconColor"
+      :name="icon"
+      size="16px"
+      :variant="iconVariant"
+    />
+    <Text v-if="autoWrap">
+      <slot />
+    </Text>
+    <slot v-else />
+  </AutoLink>
+</template>
+
+<script lang="ts" setup>
+  import { IconVariants } from '../../modules/icons'
+  import { IconTypes } from '../../modules/icon-types'
+
+
+  interface MenubarLinkProps {
+    autoWrap?: boolean // Wrap text in Text component
+    href: string // Link to navigate to
+    icon?: IconTypes // Icon to display
+    iconColor?: string // Color of icon
+    iconVariant?: IconVariants // Variant of icon
+  }
+
+  withDefaults(defineProps<MenubarLinkProps>(), {
+    autoWrap: true,
+    icon: undefined,
+    iconColor: 'primary',
+    iconVariant: 'regular'
+  })
+</script>
+
+<style lang="scss" scoped>
+  @use '../../assets/mixins.scss';
+
+  a {
+    display: grid;
+    font-size: 20px;
+    height: 40px;
+    place-items: center;
+    user-select: none;
+
+    @include mixins.rounded;
+    @include mixins.with-fade;
+    @include mixins.double-unit-inline-padding;
+
+    &.with-icon {
+      @include mixins.two-items;
+    }
+
+    i {
+      @include mixins.unit-inline-margin;
+    }
+
+    p {
+      @include mixins.no-margin;
+    }
+
+    &:hover, &.router-link-active {
+      background: var(--background-2);
+      text-decoration: none;
+    }
+
+    &:active {
+      background: var(--background-highlight);
+    }
+  }
+  </style>
