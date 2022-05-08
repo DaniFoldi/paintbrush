066b9446552b7caf48677c7deb94c3bdfafa162b
DaniFoldi <dani@stefan.hu>
2022-05-08 09:49:24 +0100

    :zap: Add button width and height to ButtonGroup

--- components/ButtonGroup.vue
+++ components/ButtonGroup.vue
@@ -1,20 +1,47 @@
 <!--!
-  @version 1.0.0
+  @version 1.1.0
   @icon arrows-in-line-vertical
   @category Layout
 -->
 
 <template>
-  <div class="button-group">
+  <div class="button-group" :class="{ height, width }">
     <slot />
   </div>
 </template>
 
+<script lang="ts" setup>
+  interface ButtonGroupProps {
+    height?: string
+    width?: string
+  }
+
+  withDefaults(defineProps<ButtonGroupProps>(), {
+    height: '',
+    width: ''
+  })
+  </script>
+
 <style lang="scss" scoped>
   div.button-group {
     display: inline-grid;
-    gap: calc(var(--unit) * 2);
-    grid-auto-columns: auto;
+    gap: var(--double-unit);
     grid-auto-flow: column;
+
+    &.width {
+      grid-auto-columns: v-bind(width);
+
+      &::v-deep(> *) {
+        min-width: unset;
+      }
+    }
+
+    &:not(.width) {
+      grid-auto-columns: auto;
+    }
+
+    &.height {
+      grid-auto-rows: v-bind(height);
+    }
   }
 </style>
