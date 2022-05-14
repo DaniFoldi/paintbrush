c394d5f317d2cb583a2358f14687caa4a2506a0f
DaniFoldi <dani@stefan.hu>
2022-05-14 14:55:30 +0100

    :zap: FocusMark now focuses an element instead

--- components/FocusMark.vue
+++ components/FocusMark.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 0.1.0
+  @version 0.2.0
   @icon hand-waving
   @category Misc
 -->
@@ -8,47 +8,76 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed } from '#imports'
+  import { computed, inject, onMounted, onBeforeUnmount, ref, useThemeColor } from '#imports'
 
 
   interface FocusMarkProps {
-    height: string
-    left: string
+    distance?: number
     off?: boolean
     opacity?: number
     rounded?: boolean | string
-    top: string
-    width: string
+    target: HTMLElement | null
   }
 
   const props = withDefaults(defineProps<FocusMarkProps>(), {
+    distance: 0,
     opacity: 0.35,
     off: false,
     rounded: false
   })
 
-  const radius = computed(() => props.rounded === true ? '50%' : (props.rounded ? props.rounded : '0'))
+  const container = inject('scrollAnchor', ref<HTMLElement | null>())
+
+  const radius = computed(() => props.rounded === true ? '50%' : (props.rounded ? useThemeColor(props.rounded).value : '0'))
   const shadow = computed(() => `rgba(0, 0, 0, ${props.opacity})`)
-  const position = computed(() => ({
-    height: props.off ? `calc(200vmax + ${props.height})` : props.height,
-    left: props.off ? `calc(-100vmax + ${props.left})` : props.left,
-    top: props.off ? `calc(-100vmax + ${props.top})` : props.top,
-    width: props.off ? `calc(200vmax + ${props.width})` : props.width
-  }))
+
+  const left = ref('0px')
+  const top = ref('0px')
+  const width = ref('100vmax')
+  const height = ref('100vmax')
+
+  onMounted(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    container.value.$el.addEventListener('scroll', draw, { passive: true })
+    draw()
+    watch(props, draw)
+  })
+
+  onBeforeUnmount(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    container.value.$el.removeEventListener('scroll', draw)
+  })
+
+  function draw() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    left.value = props.off ? `calc(-100vmax + ${props.target.$el.offsetLeft - container.value.$el.scrollLeft - props.distance}px)` : `${props.target.$el.offsetLeft - container.value.$el.scrollLeft - props.distance}px`
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    top.value = props.off ? `calc(-100vmax + ${props.target.$el.offsetTop - container.value.$el.scrollTop - props.distance}px)` : `${props.target.$el.offsetTop - container.value.$el.scrollTop - props.distance}px`
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    width.value = props.off ? `calc(200vmax + ${props.target.$el.offsetWidth + 2 * props.distance}px)` : `${props.target.$el.offsetWidth + 2 * props.distance}px`
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    height.value = props.off ? `calc(200vmax + ${props.target.$el.offsetHeight + 2 * props.distance}px)` : `${props.target.$el.offsetHeight + 2 * props.distance}px`
+  }
 </script>
 
 <style lang="scss" scoped>
   div {
     border-radius: v-bind(radius);
     box-shadow: 0 0 0 100vmax v-bind(shadow);
-    height: v-bind('position.height');
-    left: v-bind('position.left');
+    height: v-bind(height);
+    left: v-bind(left);
     pointer-events: none;
     position: fixed;
-    top: v-bind('position.top');
+    top: v-bind(top);
     // stylelint-disable-next-line plugin/no-low-performance-animation-properties -- should only have 1 such element
     transition: top .3s, height .3s, left .3s, width .3s;
-    width: v-bind('position.width');
+    width: v-bind(width);
     z-index: 10;
   }
 </style>
