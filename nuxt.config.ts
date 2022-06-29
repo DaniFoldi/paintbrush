c91551f352ffd49eddf4032f9cd52eec1058d06e
DaniFoldi <dani@stefan.hu>
2022-06-29 22:07:33 +0200

    :wrench: Add nuxt/content & register components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// #MARK(PB-2)
 
 export default defineNuxtConfig({
   // #MARK nuxt/framework #4325
   alias: {
     '@vue/devtools-api': '@vue/devtools-api'
+  },
+  components: {
+    dirs: [ '~/components' ],
+    global: true
   },
   i18n: {
     defaultLocale: 'en',
@@ -23,7 +26,8 @@ export default defineNuxtConfig({
       }
     ]
   },
-  modules: [ './modules/icons', './modules/docs', '@nuxtjs/i18n-edge', '@pinia/nuxt' ],
+  // #MARK(PB-2)
+  modules: [ './modules/icons', './modules/docs', '@nuxtjs/i18n-edge', '@pinia/nuxt', '@nuxt/content' ],
   typescript: {
     shim: false
   },
@@ -31,7 +35,8 @@ export default defineNuxtConfig({
     define: {
       __VUE_I18N_FULL_INSTALL__: false,
       __VUE_I18N_LEGACY_API__: false,
-      __INTLIFY_PROD_DEVTOOLS__: true
+      __INTLIFY_PROD_DEVTOOLS__: true,
+      __VUE_PROD_DEVTOOLS__: true
     }
   }
 })
