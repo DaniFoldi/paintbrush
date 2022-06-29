28587ac0c048c76f1b4c233f4e66e535faf645ff
DaniFoldi <dani@stefan.hu>
2022-06-29 22:08:25 +0200

    :zap: Move to content for some pages

---
+++ pages/[...content].vue
@@ -0,0 +1,5 @@
+<template>
+  <Container article padded>
+    <ContentDoc />
+  </Container>
+</template>
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Container center max padded>
-    <Text gradient title>
-      Welcome to the world of Paintbrush
-    </Text>
-  </Container>
-</template>
