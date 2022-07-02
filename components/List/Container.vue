8a43c0090501ecb2519703814faab23209b4603b
DaniFoldi <dani@stefan.hu>
2022-07-02 15:01:09 +0200

    :zap: Add ListContainer and ListItem components

---
+++ components/List/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <ol v-if="numbered">
+    <slot />
+  </ol>
+  <ul v-else>
+    <slot />
+  </ul>
+</template>
+
+<script lang="ts" setup>
+  interface ListContainerProps {
+    numbered: boolean
+  }
+
+  withDefaults(defineProps<ListContainerProps>(), {
+    numbered: false
+  })
+</script>
---
+++ components/List/Item.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <p v-if="autoWrap">
+      <slot />
+    </p>
+    <slot v-else />
+  </li>
+</template>
+
+<script lang="ts" setup>
+  interface ListItemProps {
+    autoWrap: boolean
+  }
+
+  withDefaults(defineProps<ListItemProps>(), {
+    autoWrap: false
+  })
+</script>
