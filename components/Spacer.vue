e8cea9ce642f009bdbe1d2a0fdf4789bb46201a1
DaniFoldi <dani@stefan.hu>
2022-07-05 15:58:35 +0200

    :zap: Add Spacer component

---
+++ components/Spacer.vue
@@ -0,0 +1,29 @@
+<!--!
+  @version 1.0.0
+  @category Display
+  @description An invisible spacer between elements
+  @icon arrows-out-line-vertical
+-->
+
+<template>
+  <div />
+</template>
+
+<script lang="ts" setup>
+  interface SpacerProps {
+    height?: string // Height of the spacer
+    width?: string // Width of the spacer
+  }
+
+  withDefaults(defineProps<SpacerProps>(), {
+    height: '24px',
+    width: '24px'
+  })
+</script>
+
+<style lang="scss" scoped>
+  div {
+    height: v-bind(height);
+    width: v-bind(width);
+  }
+</style>
