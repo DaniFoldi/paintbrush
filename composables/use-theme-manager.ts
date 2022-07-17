48b471062b63e58320ebde78c8c2b9b2c342cb02
DaniFoldi <dani@stefan.hu>
2022-07-17 22:09:52 +0200

    :recycle: Simplify minification

--- composables/use-theme-manager.ts
+++ composables/use-theme-manager.ts
@@ -1,21 +1,20 @@
 export default function(lightMode: string, darkMode: string) {
-  return `@media (prefers-color-scheme: light) {
-  :root:not([data-theme=light]):not([data-theme=dark]) {
-    ${lightMode}
-  }
-}
-
-:root[data-theme=light] {
-  ${lightMode}
-}
-
-@media (prefers-color-scheme: dark) {
-  :root:not([data-theme=light]):not([data-theme=dark]) {
-    ${darkMode}
-  }
-}
-
-:root[data-theme=dark] {
-  ${darkMode}
-}`
+  return `
+    @media (prefers-color-scheme: light) {
+      :root:not([data-theme=light]):not([data-theme=dark]) {
+        ${lightMode}
+      }
+    }
+    :root[data-theme=light] {
+      ${lightMode}
+    }
+    @media (prefers-color-scheme: dark) {
+      :root:not([data-theme=light]):not([data-theme=dark]) {
+        ${darkMode}
+      }
+    }
+    :root[data-theme=dark] { 
+      ${darkMode} 
+    }
+  `.replace('\n', '').replace(' ', '')
 }
