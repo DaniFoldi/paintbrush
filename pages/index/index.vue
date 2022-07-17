7064833bebdeef253785d4dd65c02e3a72340d3a
DaniFoldi <dani@stefan.hu>
2022-07-17 19:09:30 +0200

    :hammer: Fix root page not appearing

---
+++ pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Container article padded>
+    <ContentDoc>
+      <template #not-found>
+        <Container>
+          <Text title>
+            404
+          </Text>
+          <Text>
+            The page you are looking for does not exist.
+          </Text>
+          <AutoLink href="/">
+            Go to the homepage
+          </AutoLink>
+        </Container>
+      </template>
+      <template #empty>
+        <Spinner />
+      </template>
+    </ContentDoc>
+  </Container>
+</template>
