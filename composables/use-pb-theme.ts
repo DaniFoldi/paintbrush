62a8aeb8785248e422070d5bd38ce0c303bf996b
DaniFoldi <dani@stefan.hu>
2022-07-17 20:52:18 +0200

    :zap: Create usePbTheme composable

---
+++ composables/use-pb-theme.ts
@@ -0,0 +1,24 @@
+import { useTheme } from '../stores/theme'
+import { usePreferredColorScheme, createSharedComposable } from '#imports'
+
+
+export default createSharedComposable(function(): 'light' | 'dark' | 'system' {
+  const preferredTheme = useTheme()
+  if (preferredTheme.theme === 'light') {
+    return 'light'
+  } else if (preferredTheme.theme === 'dark') {
+    return 'dark'
+  }
+
+  /* global process */
+  if (process.client) {
+    const preferred = usePreferredColorScheme()
+    if (preferred.value === 'light') {
+      return 'light'
+    } else if (preferred.value === 'dark') {
+      return 'dark'
+    }
+  }
+
+  return 'system'
+})
