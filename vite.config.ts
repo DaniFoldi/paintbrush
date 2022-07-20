590eace1673b1c7d4c92ff34b885664800be54eb
DaniFoldi <dani@stefan.hu>
2022-07-20 20:34:32 +0200

    :wrench: Add visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 
 export default defineConfig({
+  plugins: [
+    visualizer({
+      template: 'sunburst'
+    })
+  ],
   test: {
     coverage: {
       all: true,
