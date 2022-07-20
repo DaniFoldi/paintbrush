0e9485dcf3a4a265bd160a55c1be356f8ceb05ee
DaniFoldi <dani@stefan.hu>
2022-07-20 17:13:04 +0200

    :recycle: Remove unused kebabcase function

--- scripts/color.ts
+++ scripts/color.ts
@@ -11,14 +11,10 @@ const hexToRgb = (color: string): number[] => {
   return [ 0, 1, 2 ].map(i => Number.parseInt(result.slice(i * 2, i * 2 + 2), 16))
 }
 
-const kebabCaseName = (name: string): string => {
-  return [ ...name ].map(c => c === c.toUpperCase() ? `-${c.toLowerCase()}` : c).join('')
-}
-
 const textColor = (backgroundColor: string, dark: string, light: string): string => {
   const [ r, g, b ] = hexToRgb(backgroundColor)
 
   return (Math.round(r * 299) + Math.round(g * 587) + Math.round(b * 114)) >= 128000 ? dark : light
 }
 
-export { kebabCaseName, resolve, textColor }
+export { resolve, textColor }
