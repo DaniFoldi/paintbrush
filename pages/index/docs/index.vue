f633d848cf3e230b97857f81753a5fcd99944fd0
DaniFoldi <dani@stefan.hu>
2022-07-20 15:50:59 +0200

    :memo: Change component docs page

--- pages/index/docs/index.vue
+++ pages/index/docs/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <Text>todo</Text>
+  <Container center max>
+    <Text title>
+      Choose a <Text font-size="inherit" gradient part>
+        component
+      </Text> on the left
+    </Text>
+  </Container>
 </template>
