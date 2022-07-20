210c2c76b67875c2a6078d5767526272a397cb0b
DaniFoldi <dani@stefan.hu>
2022-07-20 15:57:17 +0200

    :zap: Add initial codemirror editor

---
+++ components/Editor.vue
@@ -0,0 +1,20 @@
+<!--!
+  @category Form
+  @version 0.1.0
+  @icon brackets-curly
+  @description Code Editor component
+-->
+
+<!--##
+  <Editor />
+-->
+
+<template>
+  <ClientOnly>
+    <Codemirror />
+  </ClientOnly>
+</template>
+
+<script lang="ts" setup>
+  import { Codemirror } from 'vue-codemirror'
+</script>
