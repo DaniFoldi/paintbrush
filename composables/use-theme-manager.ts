2109855657860b66dbd0f7d3ebb0e85f96f625d3
DaniFoldi <dani@stefan.hu>
2022-07-20 17:12:51 +0200

    :hammer: Fix theme manager updates

--- composables/use-theme-manager.ts
+++ composables/use-theme-manager.ts
@@ -10,29 +10,25 @@ export default function(
   const schemeVariables = computed(() => Object.keys(colorScheme).map(color => `--${useKebabCase(color)}: ${colorScheme[color]};`).join(' '))
   const lightVariables = computed(() => Object.keys(lightColors).map(color => `--${useKebabCase(color)}: ${resolve(colorScheme, lightColors, color)};`).join(' '))
   const darkVariables = computed(() => Object.keys(darkColors).map(color => `--${useKebabCase(color)}: ${resolve(colorScheme, darkColors, color)};`).join(' '))
-  console.log(colorScheme)
-  console.log(lightColors)
-  console.log(schemeVariables.value)
-  console.log(lightVariables.value)
-  return `
+  return computed(() => `
     :root {
-      ${schemeVariables}
+      ${schemeVariables.value}
     }
     @media (prefers-color-scheme: light) {
       :root:not([data-theme=light]):not([data-theme=dark]) {
-        ${lightVariables}
+        ${lightVariables.value}
       }
     }
     :root[data-theme=light] {
-      ${lightVariables}
+      ${lightVariables.value}
     }
     @media (prefers-color-scheme: dark) {
       :root:not([data-theme=light]):not([data-theme=dark]) {
-        ${darkVariables}
+        ${darkVariables.value}
       }
     }
     :root[data-theme=dark] { 
-      ${darkVariables} 
+      ${darkVariables.value} 
     }
-  `.replace('\n', '').replace(' ', '')
+  `.replace('\n', '').replace(' ', ''))
 }
