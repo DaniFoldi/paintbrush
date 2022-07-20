10b574264083292129e97262ababe93251e8899b
DaniFoldi <dani@stefan.hu>
2022-07-20 14:56:48 +0200

    :zap: Use state for theme management

---
+++ composables/use-pb-color-map.ts
@@ -0,0 +1,11 @@
+export function usePbDarkColors() {
+  return useState<Record<string, string>>('pb-dark-colors', () => ({}))
+}
+
+export function usePbLightColors() {
+  return useState<Record<string, string>>('pb-light-colors', () => ({}))
+}
+
+export function usePbColorScheme() {
+  return useState<Record<string, string>>('pb-color-scheme', () => ({}))
+}
