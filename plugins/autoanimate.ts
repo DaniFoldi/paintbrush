3be9c2b4d1f7dca5b7e121a3edb5ee6bb762dcb4
DaniFoldi <dani@stefan.hu>
2022-07-23 21:38:53 +0200

    :hammer: Fix missing import

--- plugins/autoanimate.ts
+++ plugins/autoanimate.ts
@@ -1,6 +1,7 @@
 
 // eslint-disable-next-line import/no-unresolved
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { defineNuxtPlugin } from '#app'
 
 
 export default defineNuxtPlugin(nuxtApp => {
