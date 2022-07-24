b468bd55e7e368c35f37ed74c1510eba40a809e2
DaniFoldi <dani@stefan.hu>
2022-07-24 18:12:05 +0200

    :zap: Make SidebarCategory text color configurable

--- components/Sidebar/Category.vue
+++ components/Sidebar/Category.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 1.0.0
+  @version 1.1.0
   @icon sticker
   @category Navigation
   @description Categorise sidebar elements
@@ -10,8 +10,23 @@
   </Text>
 </template>
 
+<script lang="ts" setup>
+  import { computed, useThemeColor } from '#imports'
+
+
+  interface SidebarCategoryProps {
+    color?: string // Text color
+  }
+
+  const props = withDefaults(defineProps<SidebarCategoryProps>(), {
+    color: 'shadowText'
+  })
+
+  const color = computed(() => useThemeColor(props.color).value)
+</script>
+
 <style lang="scss" scoped>
   p {
-    color: var(--text-shade);
+    color: v-bind(color);
   }
 </style>
