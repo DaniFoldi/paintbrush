cd1f34e93d7446d76a2fcd698f2c152b5c9c3585
DaniFoldi <dani@stefan.hu>
2022-07-24 18:21:52 +0200

    :zap: Require external gradient for shapes

--- components/Shape/Circle.vue
+++ components/Shape/Circle.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 1.0.0
+  @version 1.1.0
   @category Visual
   @icon circle
   @description A basic circle shape
@@ -14,23 +14,17 @@
 
 
   interface CircleShapeProps {
-    color?: string // The color of the circle
-    gradient?: boolean | string // Whether the circle should have a second gradient color
     radius: string // The radius of the circle
   }
 
-  const props = withDefaults(defineProps<CircleShapeProps>(), {
-    color: 'primary',
-    gradient: false
-  })
+  const props = defineProps<CircleShapeProps>()
 
-  const color = useThemeColor(props.color)
-  const gradient = computed(() => props.gradient === true ? useThemeColor('secondary') : (props.gradient ? useThemeColor(props.gradient) : color))
+
+  const radius = computed(() => useThemeColor(props.radius).value)
 </script>
 
 <style lang="scss" scoped>
   div {
-    background-image: linear-gradient(45deg, v-bind(color), v-bind('gradient.value'));
     border-radius: 50%;
     height: v-bind(radius);
     width: v-bind(radius);
--- components/Shape/Rectangle.vue
+++ components/Shape/Rectangle.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 1.0.0
+  @version 1.1.0
   @category Visual
   @icon square
   @description A basic rectangle shape
@@ -14,27 +14,19 @@
 
 
   interface CircleShapeProps {
-    color?: string // The color of the rectangle
-    gradient?: boolean | string // Whether the rectangle should have a second gradient color
     height?: string // The height of the rectangle
     width: string // The width of the rectangle
   }
 
-  const props = withDefaults(defineProps<CircleShapeProps>(), {
-    color: 'primary',
-    height: '',
-    gradient: false
-  })
+  const props = defineProps<CircleShapeProps>()
 
-  const color = useThemeColor(props.color)
-  const gradient = computed(() => props.gradient === true ? useThemeColor('secondary') : (props.gradient ? useThemeColor(props.gradient) : color))
 
-  const size = computed(() => props.height ?? props.width)
+  const size = computed(() => useThemeColor(props.height ?? props.width).value)
+  const width = computed(() => useThemeColor(props.width).value)
 </script>
 
 <style lang="scss" scoped>
   div {
-    background-image: linear-gradient(45deg, v-bind(color), v-bind('gradient.value'));
     height: v-bind(size);
     width: v-bind(width);
   }
