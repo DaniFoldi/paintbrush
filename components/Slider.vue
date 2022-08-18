ce63c94ef82d7039efe9b6f9bedc9ff436a17c5e
DaniFoldi <dani@stefan.hu>
2022-08-18 13:53:28 +0200

    :zap: Add custom slider style

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 0.1.0
+  @version 1.0.0
   @category Form
   @icon sliders-horizontal
   @description Linear slider
@@ -7,6 +7,10 @@
 
 <!--##
   <Slider :max="20" :min="10" />
+-->
+
+<!--##
+  <Slider :max="20" :min="10" v-model="rn1" />
 -->
 
 <template>
@@ -31,7 +35,7 @@
 
   interface SliderProps {
     autoWrap?: boolean // Whether to wrap content in <Text>
-    max: number // Slider maximum value
+    max?: number // Slider maximum value
     min?: number // Slider minimum value
     modelValue?: number // Current value
     step?: number // Slider step size
@@ -41,6 +45,7 @@
 
   withDefaults(defineProps<SliderProps>(), {
     autoWrap: false,
+    max: 100,
     min: 0,
     modelValue: 0,
     step: 1
@@ -53,3 +58,52 @@
     emit('update:modelValue', value)
   }
 </script>
+
+<style lang="scss" scoped>
+  @use '~/assets/mixins';
+  @use '~/assets/common';
+  @use '~/assets/colors';
+  @mixin track {
+    @include colors.shadow-background;
+    appearance: none;
+    border-radius: 2px;
+    height: var(--unit);
+  }
+
+  @mixin thumb {
+    @include colors.primary-background;
+    @include common.circular;
+    appearance: none;
+    height: 15px;
+    width: 15px;
+  }
+
+  input[type=range] {
+    @include colors.transparent-background;
+    @include mixins.set-100-percent('width');
+
+    appearance: none;
+    overflow: visible;
+
+    &:focus {
+      outline: none;
+    }
+
+    &::-webkit-slider-runnable-track {
+      @include track;
+    }
+
+    &::-moz-range-track {
+      @include track;
+    }
+
+    &::-webkit-slider-thumb {
+      @include thumb;
+      margin-top: -5px;
+    }
+
+    &::-moz-range-thumb {
+      @include thumb;
+    }
+  }
+</style>
---
+++ pages/docs/examples/slider-1.vue
@@ -0,0 +1,10 @@
+<template>
+  <Slider v-model="rn1" :max="20" :min="10" />
+</template>
+
+<script lang="ts" setup>
+  import { ref } from '#imports'
+
+
+  const rn1 = ref<number>(0)
+</script>
