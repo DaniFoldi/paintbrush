2ab1ddc0a412ec694d01fb64be6f99b5d7fbcfed
DaniFoldi <dani@stefan.hu>
2022-08-19 10:04:09 +0200

    :hammer: Fix span inherit

--- composables/use-text-property.ts
+++ composables/use-text-property.ts
@@ -1,4 +1,10 @@
 export default function<T>(mode: string, header: [T, T, T, T, T, T] | T, p: T): T {
+  if (mode === 'span') {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return 'inherit'
+  }
+
   if (Array.isArray(header)) {
     switch (mode) {
       case 'h1': return header[0]
@@ -8,9 +14,6 @@ export default function<T>(mode: string, header: [T, T, T, T, T, T] | T, p: T): 
       case 'h5': return header[4]
       case 'h6': return header[5]
       case 'p': return p
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      case 'span': return 'inherit'
       default: return p
     }
   }
@@ -18,12 +21,5 @@ export default function<T>(mode: string, header: [T, T, T, T, T, T] | T, p: T): 
   if (mode.startsWith('h')) {
     return header
   }
-
-  switch (mode) {
-    case 'p': return p
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    case 'span': return 'inherit'
-    default: return p
-  }
+  return p
 }
