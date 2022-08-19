d87d1a72540e65c19bad6d174098f30792b60d45
DaniFoldi <dani@stefan.hu>
2022-08-19 08:37:53 +0200

    :hammer: Type fixes

--- composables/use-paintbrush-meta.ts
+++ composables/use-paintbrush-meta.ts
@@ -3,7 +3,7 @@ import { inject, useRoute } from '#imports'
 import { defaultPaintbrushPage, PaintbrushPage } from '../types/page'
 
 
-export default function(defaultPage?: PaintbrushPage): PaintbrushPage {
+export default function(defaultPage?: Partial<PaintbrushPage>): PaintbrushPage {
   return defaultPage
     ? defu(useRoute().meta.paintbrush as PaintbrushPage, defaultPage, defaultPaintbrushPage)
     : defu(useRoute().meta.paintbrush as PaintbrushPage, inject<PaintbrushPage>('pb-page'), defaultPaintbrushPage)
--- composables/use-text-property.ts
+++ composables/use-text-property.ts
@@ -1,16 +1,28 @@
-export default function<T>(mode: string, header: [T, T, T, T, T, T] | T, p: T): T | string {
-  if (mode.startsWith('h') && !Array.isArray(header)) {
+export default function<T>(mode: string, header: [T, T, T, T, T, T] | T, p: T): T {
+  if (Array.isArray(header)) {
+    switch (mode) {
+      case 'h1': return header[0]
+      case 'h2': return header[1]
+      case 'h3': return header[2]
+      case 'h4': return header[3]
+      case 'h5': return header[4]
+      case 'h6': return header[5]
+      case 'p': return p
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      case 'span': return 'inherit'
+      default: return p
+    }
+  }
+
+  if (mode.startsWith('h')) {
     return header
   }
 
   switch (mode) {
-    case 'h1': return header[0]
-    case 'h2': return header[1]
-    case 'h3': return header[2]
-    case 'h4': return header[3]
-    case 'h5': return header[4]
-    case 'h6': return header[5]
     case 'p': return p
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     case 'span': return 'inherit'
     default: return p
   }
