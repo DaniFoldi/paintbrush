ec39f49ce166c55301234ccfef4113e63f119b81
DaniFoldi <dani@stefan.hu>
2022-08-20 17:36:00 +0200

    :hammer: Fix SidebarLink imports

--- components/Sidebar/Link.vue
+++ components/Sidebar/Link.vue
@@ -1,5 +1,5 @@
 <!--!
-  @version 1.5.0
+  @version 1.5.1
   @icon link-simple
   @category Navigation
   @require phosphor-icons
@@ -24,6 +24,7 @@
 </template>
 
 <script lang="ts" setup>
+  import { computed, useThemeColor } from '#imports'
   import { IconVariants } from '../../modules/icons'
   import { IconTypes } from '../../modules/icon-types'
 
